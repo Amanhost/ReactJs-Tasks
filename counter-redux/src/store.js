b9fff8985e5483614cdf222f7555ff10a4d76d40
Aman prakash <aman.prakash@synoriq.in>
2023-08-05 22:36:10 +0530

    wefw

--- counter-redux/src/store.js
+++ counter-redux/src/store.js
@@ -1,12 +1,12 @@
-import { createStore, applyMiddleware } from "redux";
-import rootReducer from "./reducers/index";
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers/index';
 import createSagaMiddleware from 'redux-saga';
-import {watchUserInput} from './sagas/sagas'
+import { watchUserInput } from './sagas/sagas';
 
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware();
 
 // const store = createStore(rootReducer)
-const store = createStore(rootReducer,applyMiddleware(sagaMiddleware))
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 sagaMiddleware.run(watchUserInput);
 
 export default store;
